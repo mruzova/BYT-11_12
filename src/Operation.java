27791884e0945b2ace9e279084577a599d129105
s18541 <s18541>
2021-01-07 20:36:13 +0100

    Operation and User updated

--- src/Operation.java
+++ src/Operation.java
@@ -1,2 +1,44 @@
 public class Operation {
+
+    String Name;
+    String Description;
+    String Algorithm;
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getAlgorithm() {
+        return Algorithm;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        Algorithm = algorithm;
+    }
+
+    public void AddOperation(String Name, String Description, String Algorithm)
+    {
+        this.Name = Name;
+        this.Description = Description;
+        this.Algorithm = Algorithm;
+    }
+
+    public void EditOperation(String Name, String Description, String Algorithm)
+    {
+        this.Name = Name;
+        this.Description = Description;
+        this.Algorithm = Algorithm;
+    }
 }
--- src/User.java
+++ src/User.java
@@ -4,23 +4,41 @@ public abstract class User {
 
     ArrayList<Matrix> matrices = new ArrayList<>();
 
-    String username;
-    String password;
+    ArrayList<Matrix> results = new ArrayList<>();
+
+    String Username;
+    String Password;
 
     public abstract boolean addUserToDB(User user);
     public abstract boolean removeUserFromDB(User user);
 
-    public boolean addUser(String username, String password)
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setUsername(String username) {
+        this.Username = username;
+    }
+
+    public void setPassword(String password) {
+        this.Password = password;
+    }
+
+    public boolean AddUser(String username, String password)
     {
-        this.username = username;
-        this.password = password;
+        this.Username = username;
+        this.Password = password;
 
         return addUserToDB(this);
     }
 
-    public boolean removeUser(String username)
+    public boolean RemoveUser(String username)
     {
-        this.username = username;
+        this.Username = username;
 
         return removeUserFromDB(this);
     }
