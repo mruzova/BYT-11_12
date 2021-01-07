4fd654f50f8d72147c544c7e0ba183cb597f9e8c
s18541 <s18541>
2021-01-07 20:10:06 +0100

    User class added

--- src/User.java
+++ src/User.java
@@ -1,2 +1,27 @@
-public class User {
+import java.util.ArrayList;
+
+public abstract class User {
+
+    ArrayList<Matrix> matrices = new ArrayList<>();
+
+    String username;
+    String password;
+
+    public abstract boolean addUserToDB(User user);
+    public abstract boolean removeUserFromDB(User user);
+
+    public boolean addUser(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+
+        return addUserToDB(this);
+    }
+
+    public boolean removeUser(String username)
+    {
+        this.username = username;
+
+        return removeUserFromDB(this);
+    }
 }
