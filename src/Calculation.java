eccef70c0efcdd17299f81f8544054ed6eef64d4
Zacer559 <zacer559@gmail.com>
2021-01-12 15:49:50 +0100

    Class Calculation done.

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,2 +1,44 @@
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculation {
+    // Declaration of variables
+    private Timestamp timestamp;
+    private String Description;
+    private List<Integer> StepResult = new ArrayList<>();
+
+    // Getters and setters
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public List<Integer> getStepResult() {
+        return StepResult;
+    }
+
+    public void setStepResult(List<Integer> stepResult) {
+        StepResult = stepResult;
+    }
+
+    // Method which is printing our results
+    public void ShowResult(){
+        System.out.println("Results are:");
+        for (int result : this.StepResult){
+            System.out.println(result);
+        }
+
+    }
 }
