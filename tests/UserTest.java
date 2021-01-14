808a0e09a6734152a105723cb8856608cd17c9ec
s18541 <s18541>
2021-01-14 20:39:59 +0100

    UserTest.java done

--- tests/UserTest.java
+++ tests/UserTest.java
@@ -4,27 +4,37 @@ import static org.junit.Assert.*;
 
 public class UserTest {
 
+    User user = new User("username", "password");
+
     @Test
     public void getUsername() {
+        assertEquals("username", user.getUsername());
     }
 
     @Test
     public void getPassword() {
+        assertEquals("password", user.getPassword());
     }
 
     @Test
     public void setUsername() {
+        user.setUsername("newusername");
+        assertEquals("newusername", user.getUsername());
     }
 
     @Test
     public void setPassword() {
+        user.setPassword("newusername");
+        assertEquals("newusername", user.getPassword());
     }
 
     @Test
     public void addUser() {
+        //Database Connection
     }
 
     @Test
     public void deleteUser() {
+        //Database Connection
     }
 }
